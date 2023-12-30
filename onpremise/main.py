b9081687560ef9f9e58b00a87c43aa068765e621
salim laimeche <slaimeche@webexpr.fr>
2023-12-30 19:01:39 +0100

    save with bug, gonna change model, fuck yolos-tiny

--- onpremise/main.py
+++ onpremise/main.py
@@ -1,10 +1,12 @@
 # Description: This is the main file for the onPremise application
 import service.camera_service as camera_service
-
-
+from transformers import YolosImageProcessor, YolosForObjectDetection
 
 def main():
-    camera_service.run_camera()
+    model = YolosForObjectDetection.from_pretrained("onpremise/service/yolos-tiny")
+    preprocessor = YolosImageProcessor.from_pretrained("onpremise/service/yolos-tiny")
+    camera_service.run_camera(model, preprocessor)
+    camera_service.run_video(model, preprocessor, "onpremise/service/test.mp4")
 
 if __name__ == "__main__":
     main()
--- onpremise/service/blob_service.py
+++ onpremise/service/blob_service.py
@@ -1,16 +1,4 @@
-from azure.storage.blob import BlobServiceClient
 
-def upload_blob(data, filename):
-    try:
-        # Connect to the Azure storage account
-        connect_str = "DefaultEndpointsProtocol=https;AccountName=praetorianblob;AccountKey=PEv0rg+hl5Y2j4I52M6eddkBVK/1JghLJj95PfBtqC8F/T3bjKgfTwxxi6kGGPIvonrSLvvkI9Vc+AStNfJI6w==;EndpointSuffix=core.windows.net"
-        blob_client = BlobServiceClient.from_connection_string(connect_str)
-        print("\nUploading to Azure Storage as blob:\n\t" + filename)
-        # Upload the created file
-        blob_client = blob_client.get_blob_client(container="onpremvideo", blob=filename)
-        blob_client.upload_blob(data)
-            
-    except Exception as ex:
-        print('Exception:')
-        print(ex)
+
+
 
